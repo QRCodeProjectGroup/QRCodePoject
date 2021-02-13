b4f475a8e26d31375368c35a0603fa03ab5e6414
dilaraycam <dilaray.cam@gmail.com>
2021-02-13 20:11:24 +0300

    animation added

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Welcome.css';
 import arkaplan from '../../images/arkaplann.jpg'
+import 'aos/dist/aos.css'; 
+import AOS from 'aos';
+
 
 function Welcome(props){
-
+    AOS.init();
     return(
        
-<div className="row" id="gradient">
-<img id="welcome" src={arkaplan}></img>
+<div className="row" id="gradient" >
+<img id="welcome" data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="800" src={arkaplan}></img>
 </div>
 
     )
