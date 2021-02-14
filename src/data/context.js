117caff24deb734ee91c114cc74edcaaaab4f87a
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-02-14 23:56:27 +0300

    token information added

--- src/data/context.js
+++ src/data/context.js
@@ -2,11 +2,12 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 const globalContext = createContext();
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5pc2FuY2VtIiwiaWF0IjoxNjEzMzI2MTY2fQ.bAi0uH6LK98bLT7eYZK0JHm4EC9XLjYPP4Ytes1o10M'
 
 function GlobalProvider(props){
     const [product,setProduct] = useState([])
     useEffect(async()=>{
-        const response = await axios.get('https://enigmatic-temple-42944.herokuapp.com/menu');
+        const response = await axios.get(`https://enigmatic-temple-42944.herokuapp.com/api/menu?token=${token}`);
         const data = await response.data
         let products;
         data.map(item=>(products=item))
